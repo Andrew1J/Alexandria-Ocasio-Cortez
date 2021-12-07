86cc219ab1ac48a1aa24245c12bb9aa3a181e356
Andrew <andrewjuang01@gmail.com>
2021-12-07 09:36:30 -0500

    bruh

--- 7/7.py
+++ 7/7.py
@@ -1,2 +1,1 @@
-
 print(min([sum([(abs(i-num) * (abs(i-num)+1))/2 for num in [int(num) for num in [int(num) for num in open('input.txt', 'r').read().strip().split(",")]]]) for i in range(min([int(num) for num in [int(num) for num in open('input.txt', 'r').read().strip().split(",")]]),max([int(num) for num in [int(num) for num in open('input.txt', 'r').read().strip().split(",")]])+1)]))
